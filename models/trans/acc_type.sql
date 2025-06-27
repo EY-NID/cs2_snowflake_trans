69a351c3351db979d0e1fb3e5c6997aad4b6f868
Nidhi Gupta <eynidhi852@gmail.com>
2025-06-27 06:34:00 +0000

    sync

--- models/trans/acc_type.sql
+++ models/trans/acc_type.sql
@@ -1,2 +1,1 @@
-SELECT ACCOUNT_TYPE, count("Transaction_ID")
-from  MYDB.DBT_NGUPTA.JOIN group by ACCOUNT_TYPE
+SELECT "ACCOUNT_TYPE", count("Transaction_ID") from  MYDB.DBT_NGUPTA.JOIN group by "ACCOUNT_TYPE"
